800c0bd723026b77bca9e56b187baae0b87b6c93
TɴT - GɪᴛHᴜʙ (PɪxᴇʟCᴏᴍʙᴀᴛs𝟸) <163316535+StroylsMaxPixelCombats@users.noreply.github.com>
2025-01-03 20:50:09 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -13,24 +13,31 @@ Build.GetContext().BlocksSet.Value = BuildBlocksSet.Blue;
 Damage.GetContext().DamageOut.Value = true;
 
 // Создаём, команды:
-Teams.Add("Blue", "<b><size=30><color=#004e81>Ｎ</color><color=#0378a3>Г</color><color=#2e81b1≯</color><color=#2b7aaf>尺</color><color=#1a5f8f>σ</color><color=#156ea2>Ｃ</color><color=#126c97>Ⅰ</color></size></b>", new Color(0, 1, 0, 0));
+Teams.Add("Blue", "<b><size=30><color=#004e81>Ｎ</color><color=#0378a3>Г</color><color=#2e81b1≯</color><color=#2b7aaf>尺</color><color=#1a5f8f>σ</color><color=#156ea2>Ｃ</color><color=#126c97>Ⅰ</color></size></b>", new Color(0, 1, 1, 0));
 Teams.Add("Red", "<b><size=30><color=#b43b00>S</color><color=#b44400>丅</color><color=#b44d00>尺</color><color=#b45600>σ</color><color=#b45f00>l</color><color=#b46800>Ƭ</color><color=#b47100>Έ</color><color=#b47a00>刀</color><color=#b48300>Ⅰ</color></size></b>", new Color(1, 0.7, 0, 0));
-// разрешаем вход в команды по запросу
-Teams.OnRequestJoinTeam.Add(function(player,team){team.Add(player);});
-// спавн по входу в команду
-Teams.OnPlayerChangeTeam.Add(function(player){ player.Spawns.Spawn()});
+var PlayersTeam = Teams.Get("Blue");
+var StroiteliTeam = Teams.Get("Red");
+PlayersTeam.Spawns.SpawnPointsGroups.Add(1);
+PlayersTeam.Build.BlocksSet.Value = BuildBlocksSet.Blue;
+StroiteliTeam.Spawns.SpawnPointsGroups.Add(2);
+StroiteliTeam.Build.BlocksSet.Value = BuildBlocksSet.AllClear;
 
-// задаем подсказку
-Ui.getContext().Hint.Value = "Hint/BuildBase";
+// Входы, в - режим:
+Teams.OnRequestJoinTeam.Add(function(Player,Team){Team.Add(Player);});
+Teams.OnPlayerChangeTeam.Add(function(Player){ Player.Spawns.Spawn()});
 
-// конфигурация инвентаря
-var roomInventory = Inventory.GetContext();
-roomInventory.Main.Value = false;
-roomInventory.Secondary.Value = false;
-roomInventory.Melee.Value = true;
-roomInventory.Explosive.Value = false;
-roomInventory.Build.Value = true;
-roomInventory.BuildInfinity.Value = true;
+// Настройка:
+Ui.GetContext().Hint.Value = `Здравствуйте: ${Player.NickName}$!`;
+ Teams.Get("PlayersTeam").Inventory.Main.Value = false;
+ Teams.Get("PlayersTeam").Inventory.Secondary.Value = false;
+ Teams.Get("PlayersTeam").Inventory.Melee.Value = false;
+ Teams.Get("PlayersTeam").Inventory.Explosive.Value = false;
+ Teams.Get("PlayersTeam").Inventory.Build.Value = false;
+Teams.Get("PlayersTeam").Properties.Get("AdminLeaderBoard").Value = "<b><i><color>Red>✖</a></i></b>";
+Teams.Get("PlayersTeam").Properties.Get("Status").Value = "<b><color=Lime>ＮГ̸尺σＣ</a></b>";
+
+
+
 
 // моментальный спавн
 Spawns.GetContext().RespawnTime.Value = 0;
