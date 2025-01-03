dfadb788a9580e3d21686955198c63e177af678a
TɴT - GɪᴛHᴜʙ (PɪxᴇʟCᴏᴍʙᴀᴛs𝟸) <163316535+StroylsMaxPixelCombats@users.noreply.github.com>
2025-01-03 21:31:42 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -11,6 +11,7 @@ BreackGraph.BreackAll = true;
 Ui.GetContext().QuadsCount.Value = true;
 Build.GetContext().BlocksSet.Value = BuildBlocksSet.Blue;
 Damage.GetContext().DamageOut.Value = true;
+Spawns.GetContext().RespawnTime.Value = 5;
 
 // Создаём, команды:
 Teams.Add("Blue", "<b><size=30><color=#004e81>Ｎ</color><color=#0378a3>Г</color><color=#2e81b1≯</color><color=#2b7aaf>尺</color><color=#1a5f8f>σ</color><color=#156ea2>Ｃ</color><color=#126c97>Ⅰ</color></size></b>", new Color(0, 1, 1, 0));
@@ -26,7 +27,7 @@ StroiteliTeam.Build.BlocksSet.Value = BuildBlocksSet.AllClear;
 Teams.OnRequestJoinTeam.Add(function(Player,Team){Team.Add(Player);});
 Teams.OnPlayerChangeTeam.Add(function(Player){ Player.Spawns.Spawn()});
 
-// Настройка:
+// Настройка, команды - игроки:
 Ui.GetContext().Hint.Value = `Здравствуйте: ${Player.NickName}$!`;
  Teams.Get("PlayersTeam").Inventory.Main.Value = false;
  Teams.Get("PlayersTeam").Inventory.Secondary.Value = false;
@@ -34,10 +35,38 @@ Ui.GetContext().Hint.Value = `Здравствуйте: ${Player.NickName}$!`;
  Teams.Get("PlayersTeam").Inventory.Explosive.Value = false;
  Teams.Get("PlayersTeam").Inventory.Build.Value = false;
 Teams.Get("PlayersTeam").Properties.Get("AdminLeaderBoard").Value = "<b><i><color>Red>✖</a></i></b>";
-Teams.Get("PlayersTeam").Properties.Get("Status").Value = "<b><color=Lime>ＮГ̸尺σＣ</a></b>";
+Teams.Get("PlayersTeam").Properties.Get("Status").Value = "<b><size=30><color=#2b7aaf>ＮГ̸尺σＣⅠ</size></b>";
+Teams.Get("PlayersTeam").Properties.Get("BanLeaderBoard").Value = "<b><i><color=Lime>✖</a></i></b>";
+// ЛидерБорды:
+LeaderBoard.PlayerLeaderBoardValues = [
+ new DisplayValueHeader("AdminLeaderBoard", "<b><color=Red>Админ:</a></b>", "<b><color=Red>Админ:</a></b>"),
+ new DisplayValueHeader("BanLeaderBoard", "<b><color=Orange>Забанен:</a></b>", "<b><color=Orange>Забанен:</a></b>"),
+ new DisplayValueHeader("Status", "<b><color=Blue>Статус:</a></b>", "<b><color=Blue>Статус:</a></b>")
+];
+
+// Консоль:
+Teams.OnRequestJoinTeam.Add(function(Player,Team){
+ function SetAdminka(Teams) {
+Teams.Get("StroiteliTeam").Inventory.Main.Value = true;
+Teams.Get("StroiteliTeam").Inventory.Secondary.Value = true;
+Teams.Get("StroiteliTeam").Inventory.Melee.Value = true;
+Teams.Get("StroiteliTeam").Inventory.Explosive.Value = true;
+Teams.Get("StroiteliTeam").Inventory.Build.Value = true;
+Teams.Get("StroiteliTeam").Inventory.MainInfinity.Value = true;
+Teams.Get("StroiteliTeam").Inventory.SecondaryInfinity.Value = true;
+Teams.Get("StroiteliTeam").Inventory.ExplosiveInfinity.Value = true;
+Teams.Get("StroiteliTeam").Inventory.BuildInfinity.Value = true;
+Teams.Get("StroiteliTeam").Build.FlyEnable.Value = true;
+Teams.Get("StroiteliTeam").Damage.DamageIn.Value = false;
 
 
 
 
-// моментальный спавн
-Spawns.GetContext().RespawnTime.Value = 0;
+
+  
+
+
+
+ 
+ 
+
